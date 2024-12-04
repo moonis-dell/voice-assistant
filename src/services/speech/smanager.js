8925678e7a5f1c0bcd13432294a08a8475fb283d
moonis-dell <moonis-dell@outlook.com>
2024-12-04 20:51:19 +0530

    timer from 2 to 3

--- src/services/speech/smanager.js
+++ src/services/speech/smanager.js
@@ -13,7 +13,7 @@ export class SpeechManager extends EventEmitter {
             transcript: {
                 accumulated: '',
                 timer: null,
-                waitTime: 2000, // 2 seconds
+                waitTime: 3000, // 3 seconds
                 lastTranscriptTime: null
             },
             tts: {
@@ -65,7 +65,7 @@ export class SpeechManager extends EventEmitter {
 
         this.state.transcript.lastTranscriptTime = Date.now();
         console.log(`[${this.getTimestamp()}] Accumulated transcript: "${this.state.transcript.accumulated}"`);
-        console.log(`[${this.getTimestamp()}] Starting 2-second timer...`);
+        console.log(`[${this.getTimestamp()}] Starting 3-second timer...`);
 
         this.resetTranscriptTimer();
     }
